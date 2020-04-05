f5c64a7bc430b6c91c34cfc1532a1006c554fe2b
Dewald <Dewald@Dewalds-MacBook-Pro.local>
2020-04-05 15:55:52 +0200

    added axios and semantic

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,14 @@ namespace API
             });
             services.AddControllers();
 
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +57,7 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
