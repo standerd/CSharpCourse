aeaad6c03fa9b4a25b09df7bb66fd24d522f995d
Dewald <Dewald@Dewalds-MacBook-Pro.local>
2020-04-14 16:41:20 +0200

    end of first section for api and client

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Logging;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using MediatR;
+using Application.Activities;
 
 namespace API
 {
@@ -41,6 +43,8 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
 
         }
 
